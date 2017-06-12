d57d2312002437d04f49f1bfb51be9a530c56a21
Daniel Urdaneta <dsurdaneta@gmail.com>
2017-06-12 16:06:21 +0200

    more test methods

--- BusinessDaysTest/Calculator.cs
+++ BusinessDaysTest/Calculator.cs
@@ -8,12 +8,30 @@ namespace BusinessDays.Test
     public class Calculator
     {
         [TestMethod]
-        public void GetBusinessDaysCountNoHolidays()
+        public void BussinesDays_GetBusinessDaysCountNoHolidaysFile()
         {
             DateTime starDate = new DateTime(2001,5,26);
             DateTime enDateTime = new DateTime(2001,6,11);
             var result = BusinessDaysCalculator.GetBusinessDaysCount(starDate, enDateTime);
             Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void BussinesDays_AddBusinessDaysNoHolidaysFile()
+        {
+            DateTime starDate = new DateTime(2001, 5, 26);
+            var result = BusinessDaysCalculator.AddBusinessDays(starDate, 15);
+            DateTime expectedDate = new DateTime(2001,6,15);
+            Assert.AreEqual(expectedDate, result);
+        }
+
+        [TestMethod]
+        public void BussinesDays_AddBusinessDaysWithHolidaysCounter()
+        {
+            DateTime starDate = new DateTime(2001, 5, 26);
+            var result = BusinessDaysCalculator.AddBusinessDays(starDate, 15,3);
+            DateTime expectedDate = new DateTime(2001, 6, 18);
+            Assert.AreEqual(expectedDate, result);
         }
     }
 }
