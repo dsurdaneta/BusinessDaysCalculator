25c7ac061b35cb5b296557dff32244b2590993b5
Daniel Urdaneta <dsurdaneta@gmail.com>
2018-04-23 12:36:00 +0200

    use of 'var'

--- BusinessDaysTest/CalculatorTest.cs
+++ BusinessDaysTest/CalculatorTest.cs
@@ -6,58 +6,58 @@ using DsuDev.BusinessDays.Constants;
 namespace DsuDev.BusinessDays.Test
 {
 	[TestClass]
-    public class CalculatorTest
-    {
+	public class CalculatorTest
+	{
 		[TestMethod]
 		public void BusinessDays_CalculatorObjIsNotNull()
 		{
-            //Act
+			//Act
 			var sut = new BusinessDaysCalculator();
-            //Assert
+			//Assert
 			Assert.IsNotNull(sut);
 		}		
 
 		[TestMethod]
 		public void BusinessDays_FileExtensionObjIsNotNull()
 		{
-            //Act
+			//Act
 			var sut = new FileExtension();
-            //Assert
+			//Assert
 			Assert.IsNotNull(sut);
 		}
 
-        [TestMethod]
-        public void BusinessDays_GetBusinessDaysCountNoHolidaysFile()
-        {
-            //Arrange
-            int year = 2001;
-            DateTime startDate = new DateTime(year,5,26);
-            DateTime expectedDate = new DateTime(year,6,11);
-			//Act
-            var sut = BusinessDaysCalculator.GetBusinessDaysCount(startDate, expectedDate);
-			//Assert
-            Assert.AreEqual(10, sut);
-        }
-
-        [TestMethod]
-        public void BusinessDays_AddBusinessDaysNoHolidaysFile()
-        {
-            //Arrange
-            int year = 2001;
-            DateTime startDate = new DateTime(year, 5, 26);
-			DateTime expectedDate = new DateTime(year,6,15);
-			//Act
-            var sut = BusinessDaysCalculator.AddBusinessDays(startDate, 15);
-            //Assert
-            Assert.AreEqual(expectedDate, sut);
-        }
-
-        [TestMethod]
-        public void BusinessDays_AddBusinessDaysWithHolidaysCounter()
+		[TestMethod]
+		public void BusinessDays_GetBusinessDaysCountNoHolidaysFile()
 		{
 			//Arrange
-			DateTime starDate = new DateTime(2001, 5, 26);
-			DateTime expectedDate = new DateTime(2001, 6, 18);
+			const int year = 2001;
+			var startDate = new DateTime(year, 5, 26);
+			var expectedDate = new DateTime(year, 6, 11);
+			//Act
+			var sut = BusinessDaysCalculator.GetBusinessDaysCount(startDate, expectedDate);
+			//Assert
+			Assert.AreEqual(10, sut);
+		}
+
+		[TestMethod]
+		public void BusinessDays_AddBusinessDaysNoHolidaysFile()
+		{
+			//Arrange
+			const int year = 2001;
+			var startDate = new DateTime(year, 5, 26);
+			var expectedDate = new DateTime(year, 6, 15);
+			//Act
+			var sut = BusinessDaysCalculator.AddBusinessDays(startDate, 15);
+			//Assert
+			Assert.AreEqual(expectedDate, sut);
+		}
+
+		[TestMethod]
+		public void BusinessDays_AddBusinessDaysWithHolidaysCounter()
+		{
+			//Arrange
+			var starDate = new DateTime(2001, 5, 26);
+			var expectedDate = new DateTime(2001, 6, 18);
 			//Act
 			var sut = BusinessDaysCalculator.AddBusinessDays(starDate, 15, 3);
 			//Assert
@@ -67,11 +67,11 @@ namespace DsuDev.BusinessDays.Test
 		[TestMethod]
 		public void BusinessDays_AddBusinessDaysFromList()
 		{
-            //Arrange
-            int year = 2001;
-            List<Holiday> holidays = new List<Holiday>();			
-			DateTime starDate = new DateTime(year, 4, 27);
-			DateTime expectedDate = new DateTime(year, 5, 17);
+			//Arrange
+			const int year = 2001;
+			var holidays = new List<Holiday>();			
+			var starDate = new DateTime(year, 4, 27);
+			var expectedDate = new DateTime(year, 5, 17);
 			//Act
 			holidays.Add(HolidayTests.GetWorkersDay(year));
 			var sut = BusinessDaysCalculator.AddBusinessDays(starDate, 15, holidays);
@@ -82,11 +82,11 @@ namespace DsuDev.BusinessDays.Test
 		[TestMethod]
 		public void BusinessDays_GetBusinessDaysCountFromList()
 		{
-            //Arrange
-            int year = 2001;
-			List<Holiday> holidays = new List<Holiday>();
-			DateTime startDate = new DateTime(year, 4, 25);
-			DateTime expectedDate = new DateTime(year, 5, 9);
+			//Arrange
+			const int year = 2001;
+			var holidays = new List<Holiday>();
+			var startDate = new DateTime(year, 4, 25);
+			var expectedDate = new DateTime(year, 5, 9);
 			//Act
 			holidays.Add(HolidayTests.GetWorkersDay(year));
 			var sut = BusinessDaysCalculator.GetBusinessDaysCount(startDate, expectedDate, holidays);
