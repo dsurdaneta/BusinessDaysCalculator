022e7e24da2d5b8d90ba3c5fc65dc8237a9cd9e2
dsurdaneta <dsurdaneta@gmail.com>
2019-05-21 14:17:09 +0200

    add FilePathInfoBuilder

---
+++ DsuDev.BusinessDays.Tools/FluentBuilders/FilePathInfoBuilder.cs
@@ -0,0 +1,56 @@
+﻿using DsuDev.BusinessDays.Domain.Entities;
+
+namespace DsuDev.BusinessDays.Tools.FluentBuilders
+{
+    public class FilePathInfoBuilder
+    {
+        private string currentFolder;
+        private string fileName;
+        private string ext;
+        private bool isAbsolutePath;
+
+        public FilePathInfoBuilder Create()
+        {
+            this.currentFolder = string.Empty;
+            this.fileName = string.Empty;
+            this.ext = string.Empty;
+            this.isAbsolutePath = false;
+            return this;
+        }
+
+        public FilePathInfoBuilder WithFileName(string name)
+        {
+            this.fileName = name;
+            return this;
+        }
+
+        public FilePathInfoBuilder WithFolder(string folder)
+        {
+            this.currentFolder = folder;
+            return this;
+        }
+        
+        public FilePathInfoBuilder WithExtension(string extension)
+        {
+            this.ext = extension;
+            return this;
+        }
+
+        public FilePathInfoBuilder WithIsAbsolutePath(bool isAbsolute)
+        {
+            this.isAbsolutePath = isAbsolute;
+            return this;
+        }
+
+        public FilePathInfo Build()
+        {
+            return new FilePathInfo
+            {
+                Folder = this.currentFolder,
+                FileName = this.fileName,
+                Extension = this.ext,
+                IsAbsolutePath = this.isAbsolutePath
+            };
+        }
+    }
+}
