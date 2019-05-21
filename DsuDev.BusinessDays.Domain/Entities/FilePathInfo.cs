5cbfe17f4365fd059da6cba72a60553f5856eae3
dsurdaneta <dsurdaneta@gmail.com>
2019-05-21 09:46:54 +0200

    added FilePathInfo Entity

---
+++ DsuDev.BusinessDays.Domain/Entities/FilePathInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace DsuDev.BusinessDays.Domain.Entities
+{
+    public class FilePathInfo
+    {
+        public string Folder { get; set; }
+        public string FileName { get; set; }
+        public string Extension { get; set; }
+    }
+}
