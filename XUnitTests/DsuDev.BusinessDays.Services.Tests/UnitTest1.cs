e5bd4c501ed5076e4cb09e09ba4cbbe60e8c6cad
Daniel urdaneta <dsurdaneta@gmail.com>
2019-06-04 09:42:29 +0200

    add Services.Test project to xUnit folder

---
+++ XUnitTests/DsuDev.BusinessDays.Services.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DsuDev.BusinessDays.Services.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
