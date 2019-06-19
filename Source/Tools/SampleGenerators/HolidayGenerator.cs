a3bd9ab9fa83f285c105ccb11fbc3e20105d3487
Daniel Urdaneta <dsurdaneta@gmail.com>
2019-06-19 09:56:45 +0200

    renamed property

--- Source/Tools/SampleGenerators/HolidayGenerator.cs
+++ Source/Tools/SampleGenerators/HolidayGenerator.cs
@@ -5,11 +5,11 @@ namespace DsuDev.BusinessDays.Tools.SampleGenerators
 {
     public class HolidayGenerator
     {
-        private static readonly HolidayBuilder holidayBuilder = new HolidayBuilder();
+        private static readonly HolidayBuilder HolidayBuilder = new HolidayBuilder();
 
         public static Holiday CreateHoliday(int year = 2001, string description = " ", string name = "Workers Day")
         {
-            return holidayBuilder
+            return HolidayBuilder
                 .Create()
                 .WithDate(year, 5, 1)
                 .WithName(name)
