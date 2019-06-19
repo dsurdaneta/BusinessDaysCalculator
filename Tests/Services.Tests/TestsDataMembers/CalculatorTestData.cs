9d69c05defc967b569140451384777efe6c1f10a
Daniel Urdaneta <dsurdaneta@gmail.com>
2019-06-19 10:39:50 +0200

    Add DataClass

---
+++ Tests/Services.Tests/TestsDataMembers/CalculatorTestData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DsuDev.BusinessDays.Domain.Entities;
+using DsuDev.BusinessDays.Services.FileReaders;
+using Moq;
+
+namespace DsuDev.BusinessDays.Services.Tests.TestsDataMembers
+{
+    public class CalculatorTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            var mockFileReadingManager = new Mock<IFileReadingManager>();
+            var path = new FilePathInfo();
+            yield return new object[] { null, mockFileReadingManager.Object };
+            yield return new object[] { path, null };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
