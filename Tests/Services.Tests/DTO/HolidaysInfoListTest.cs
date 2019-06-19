cf9f48e07bcf4f711d489262e86719e9a428bf1e
Urdaneta Daniel (ERNI) <daniel.urdaneta@erni-espana.es>
2019-06-19 09:54:45 +0200

    removed redundant test

--- Tests/Services.Tests/DTO/HolidaysInfoListTest.cs
+++ Tests/Services.Tests/DTO/HolidaysInfoListTest.cs
@@ -7,16 +7,6 @@ namespace DsuDev.BusinessDays.Services.Tests.DTO
     public class HolidaysInfoListTest
     {
         [Fact]
-        public void InfoList_WithVoidConstructorHolidayInfoListObjIsNotNull()
-        {
-            //Act
-            var sut = new HolidaysInfoList();
-            
-            //Assert
-            sut.Should().NotBeNull();
-        }
-
-        [Fact]
         public void InfoList_WithVoidConstructorHolidayInfoListNotNullHolidaysList()
         {
             //Act
