0923cdaa5d84edca966d9c531f33ee9f97a03ee7
Daniel Urdaneta <dsurdaneta@gmail.com>
2019-07-01 12:56:42 +0200

    Add a method to geneerate a Sample List of random holidays

--- Source/Tools/SampleGenerators/HolidayGenerator.cs
+++ Source/Tools/SampleGenerators/HolidayGenerator.cs
@@ -1,4 +1,5 @@
-﻿using DsuDev.BusinessDays.Domain.Entities;
+﻿using System.Collections.Generic;
+using DsuDev.BusinessDays.Domain.Entities;
 using DsuDev.BusinessDays.Tools.FluentBuilders;
 
 namespace DsuDev.BusinessDays.Tools.SampleGenerators
@@ -7,14 +8,33 @@ namespace DsuDev.BusinessDays.Tools.SampleGenerators
     {
         private static readonly HolidayBuilder HolidayBuilder = new HolidayBuilder();
 
-        public static Holiday CreateHoliday(int year = 2001, string description = " ", string name = "Workers Day")
+        public static Holiday CreateHoliday(int year = 2001, int month = 5, int day = 1, string description = " ", string name = "Workers Day")
         {
             return HolidayBuilder
                 .Create()
-                .WithDate(year, 5, 1)
+                .WithDate(year, month, day)
                 .WithName(name)
                 .WithDescription(description)
                 .Build();
+        }
+
+        public static List<Holiday> CreateHolidays(int amount, int baseYear)
+        {
+            var holidays = new List<Holiday>();
+            
+            for (int i = 0; i < amount; i++)
+            {
+                var randomHoliday = CreateHoliday(
+                    baseYear,
+                    RandomValuesGenerator.RandomInt(1, 12),
+                    RandomValuesGenerator.RandomInt(1, 28),
+                    RandomValuesGenerator.RandomString(30),
+                    RandomValuesGenerator.RandomString(10));
+
+                holidays.Add(randomHoliday);
+            }
+
+            return holidays;
         }
     }
 }
