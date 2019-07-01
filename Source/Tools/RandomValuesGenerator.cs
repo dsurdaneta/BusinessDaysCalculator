56068173ee8a16ce330b8603d9cf9047ef591264
Daniel Urdaneta <dsurdaneta@gmail.com>
2019-07-01 12:03:10 +0200

    add random generator class (for testing)

---
+++ Source/Tools/RandomValuesGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace DsuDev.BusinessDays.Tools
+{
+    public static class RandomValuesGenerator
+    {
+        const string Chars = "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public static string RandomString(int size)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                builder.Append(Enumerable.Range(0, size)
+                    .Select(x => Chars[random.Next(1, Chars.Length)]));
+            }
+
+            return builder.ToString();
+        }
+
+        public static int RandomInt(int min, int max)
+        {
+            Random random = new Random();
+            return min > 0 ? random.Next(min, max) : random.Next(max);
+        }
+    }
+}
