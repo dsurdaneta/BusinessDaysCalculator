119a9a0b55d48a3cdf8467eb5cdb41ade87d654e
Daniel Urdaneta <dsurdaneta@gmail.com>
2019-07-01 12:57:38 +0200

    add unit tests for FileReadingManager.ReadHolidaysFile

--- Tests/Services.Tests/FileReaders/FileReadingManagerTests.cs
+++ Tests/Services.Tests/FileReaders/FileReadingManagerTests.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DsuDev.BusinessDays.Services.Constants;
 using DsuDev.BusinessDays.Services.FileReaders;
 using DsuDev.BusinessDays.Services.Tests.TestsDataMembers;
+using DsuDev.BusinessDays.Tools;
+using DsuDev.BusinessDays.Tools.FluentBuilders;
+using DsuDev.BusinessDays.Tools.SampleGenerators;
+using FluentAssertions;
 using Moq;
+using System;
 using Xunit;
 
 namespace DsuDev.BusinessDays.Services.Tests.FileReaders
@@ -34,6 +37,48 @@ namespace DsuDev.BusinessDays.Services.Tests.FileReaders
             //Assert
             Assert.Throws<ArgumentNullException>(
                 () => new FileReadingManager(jsonReader, xmlReader, csvReader, customReader));
+        }
+
+        [Fact]
+        public void ReadHolidaysFile_When_ValidFileExtension_ReturnsHolidayList()
+        {
+            // Arrange
+            var expectedAmount = 4;
+            jsonMock.Setup(setup => setup.GetHolidaysFromFile(It.IsAny<string>()))
+                .Returns(HolidayGenerator.CreateHolidays(expectedAmount, 2010));
+            
+            var fileReading = new FileReadingManager(jsonMock.Object, xmlMock.Object, csvMock.Object, txtMock.Object);
+            var pathInfo = new FilePathInfoBuilder().Create()
+                .WithFolder(Resources.ContainingFolderName)
+                .WithFileName(Resources.FileName)
+                .WithExtension(FileExtension.Json)
+                .Build();
+
+            // Act
+            var sut = fileReading.ReadHolidaysFile(pathInfo);
+
+            // Assert
+            sut.Should().NotBeNull();
+            sut.Count.Should().Be(expectedAmount);
+        }
+        
+        [Fact]
+        public void ReadHolidaysFile_When_InvalidFileExtension_ThrowsException()
+        {
+            // Arrange
+            var fileReading = new FileReadingManager(jsonMock.Object, xmlMock.Object, csvMock.Object, txtMock.Object);
+            var ext = RandomValuesGenerator.RandomString(3);
+            var pathInfo = new FilePathInfoBuilder().Create()
+                .WithFolder(Resources.ContainingFolderName)
+                .WithFileName(Resources.FileName)
+                .WithExtension(ext)
+                .Build();
+
+            // Act
+            Action action = () => fileReading.ReadHolidaysFile(pathInfo);
+
+            // Act
+            action.Should().Throw<InvalidOperationException>();
         }
     }
 }
