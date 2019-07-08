527a7a08ff6c825ece7b1ad5bfbf4216081ee195
Daniel Urdaneta <dsurdaneta@gmail.com>
2019-07-08 11:00:15 +0200

    increased coverage of FileReaders

---
+++ Tests/Services.Tests/FileReaders/JsonReaderTests.cs
@@ -0,0 +1,48 @@
+﻿using DsuDev.BusinessDays.Services.FileReaders;
+using DsuDev.BusinessDays.Tools;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace DsuDev.BusinessDays.Services.Tests.FileReaders
+{
+    public class JsonReaderTests
+    {
+        [Fact]
+        public void JsonHolidateReader_When_new_Then_hasExpectedProperties()
+        {
+            // Act
+            var reader = new JsonHolidayReader();
+
+            // Assert
+            reader.Holidays.Should().NotBeNull();
+            reader.Holidays.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void Json_GetHolidaysFromFile_When_EmptyPath_Then_ThrowException()
+        {
+            // Arrange
+            var reader = new JsonHolidayReader();
+            // Act
+            Action action = () => reader.GetHolidaysFromFile(string.Empty);
+
+            // Assert
+            action.Should().Throw<ArgumentException>();
+        }
+        
+        [Fact]
+        public void Json_GetHolidaysFromFile_When_WrongFileExtension_Then_ThrowException()
+        {
+            // Arrange
+            var reader = new JsonHolidayReader();
+            var path = RandomValuesGenerator.RandomString(6);
+
+            // Act
+            Action action = () => reader.GetHolidaysFromFile(path);
+
+            // Assert
+            action.Should().Throw<InvalidOperationException>();
+        }
+    }
+}
---
+++ Tests/Services.Tests/FileReaders/XmlReaderTests.cs
@@ -0,0 +1,48 @@
+﻿using DsuDev.BusinessDays.Services.FileReaders;
+using DsuDev.BusinessDays.Tools;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace DsuDev.BusinessDays.Services.Tests.FileReaders
+{
+    public class XmlReaderTests
+    {
+        [Fact]
+        public void XmlHolidateReader_When_new_Then_hasExpectedProperties()
+        {
+            // Act
+            var reader = new XmlHolidayReader();
+
+            // Assert
+            reader.Holidays.Should().NotBeNull();
+            reader.Holidays.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void Xml_GetHolidaysFromFile_When_EmptyPath_Then_ThrowException()
+        {
+            // Arrange
+            var reader = new XmlHolidayReader();
+            // Act
+            Action action = () => reader.GetHolidaysFromFile(string.Empty);
+
+            // Assert
+            action.Should().Throw<ArgumentException>();
+        }
+        
+        [Fact]
+        public void Xml_GetHolidaysFromFile_When_WrongFileExtension_Then_ThrowException()
+        {
+            // Arrange
+            var reader = new XmlHolidayReader();
+            var path = RandomValuesGenerator.RandomString(6);
+
+            // Act
+            Action action = () => reader.GetHolidaysFromFile(path);
+
+            // Assert
+            action.Should().Throw<InvalidOperationException>();
+        }
+    }
+}
