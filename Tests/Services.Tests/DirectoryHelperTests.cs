c9f7bc47a814ca7dfffac990d0ea907634951495
Daniel Urdaneta <dsurdaneta@gmail.com>
2019-07-08 10:49:17 +0200

    add DirectoryHelperTests

---
+++ Tests/Services.Tests/DirectoryHelperTests.cs
@@ -0,0 +1,38 @@
+﻿using DsuDev.BusinessDays.Tools.FluentBuilders;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace DsuDev.BusinessDays.Services.Tests
+{
+    public class DirectoryHelperTests
+    {
+        [Fact]
+        public void ValidateFilePathInfo_When_PathInfoIsNull_ThenThrowException()
+        {
+            // Act
+            Action action = () => DirectoryHelper.ValidateFilePathInfo(null);
+
+            // Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Theory]
+        [InlineData(null, "hello")]
+        [InlineData("hello", "")]
+        public void ValidateFilePathInfo_NotValidPath(string filename, string ext)
+        {
+            // Arrange
+            var pathInfo = new FilePathInfoBuilder().Create()
+                .WithFileName(filename)
+                .WithExtension(ext)
+                .Build();
+
+            // Act
+            Action action = () => DirectoryHelper.ValidateFilePathInfo(pathInfo);
+
+            // Assert
+            action.Should().Throw<ArgumentException>();
+        }
+    }
+}
