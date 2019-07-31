a161a3e787e7936f45ea5c72dabc9fa79f250fae
Daniel Urdaneta <dsurdaneta@gmail.com>
2019-07-31 13:43:24 +0200

    added Ctor

--- Source/Domain/Entities/FilePathInfo.cs
+++ Source/Domain/Entities/FilePathInfo.cs
@@ -9,5 +9,13 @@
         public string FileName { get; set; }
         public string Extension { get; set; }
         public bool IsAbsolutePath { get; set; }
+
+        public FilePathInfo()
+        {
+            Folder = string.Empty;
+            FileName = string.Empty;
+            Extension = string.Empty;
+            IsAbsolutePath = false;
+        }
     }
 }
