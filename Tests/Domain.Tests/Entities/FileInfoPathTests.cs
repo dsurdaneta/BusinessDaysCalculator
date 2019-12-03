cfc746b7d0642537fd6c0befaaeb75558857bfc0
Daniel Urdaneta <dsurdaneta@gmail.com>
2019-12-03 15:21:12 +0100

    increased coverage

---
+++ Tests/Domain.Tests/Entities/FileInfoPathTests.cs
@@ -0,0 +1,23 @@
+﻿using DsuDev.BusinessDays.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace DsuDev.BusinessDays.Domain.Tests.Entities
+{
+    public class FileInfoPathTests
+    {
+        [Fact]
+        public void FileInfoPath_WithVoidContainer()
+        {
+            // Act
+            var sut = new FilePathInfo();
+
+            // Assert
+            sut.Should().NotBeNull();
+            sut.IsAbsolutePath.Should().BeFalse();
+            sut.FileName.Should().BeEmpty();
+            sut.Extension.Should().BeEmpty();
+            sut.Folder.Should().BeEmpty();
+        }
+    }
+}
