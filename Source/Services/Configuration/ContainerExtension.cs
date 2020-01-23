5b3f5bd2e33d6bb907c3529cf2b25cd7234729d5
Daniel Urdaneta <dsurdaneta@gmail.com>
2020-01-23 11:45:55 +0100

    Added methods to register all the services in ContainerExtensions class

--- Source/Services/Configuration/ContainerExtension.cs
+++ Source/Services/Configuration/ContainerExtension.cs
@@ -1,15 +1,27 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Linq;
+﻿using DsuDev.BusinessDays.DataAccess;
+using DsuDev.BusinessDays.DataAccess.SQLite;
 using DsuDev.BusinessDays.Services.FileReaders;
+using DsuDev.BusinessDays.Services.Interfaces;
 using DsuDev.BusinessDays.Services.Interfaces.FileReaders;
-using SimpleInjector;
 using Microsoft.Extensions.Configuration;
+using SimpleInjector;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using DbModels = DsuDev.BusinessDays.DataAccess.Models;
 
 namespace DsuDev.BusinessDays.Services.Configuration
 {
     [ExcludeFromCodeCoverage]
     public static class ContainerExtension
     {
+        public static Container RegisterAll(this Container container)
+        {
+            RegisterDataAccess(container);
+            RegisterFileReaders(container);
+            RegisterServices(container);
+            return container;
+        }
+
         /// <summary>
         /// Registers all the needed FileReader classes into the given container
         /// </summary>
@@ -23,6 +35,31 @@ namespace DsuDev.BusinessDays.Services.Configuration
             container.Register<ICustomTxtReader,CustomTxtHolidayReader>();
             container.Register<IFileReadingManager,FileReadingManager>();
 
+            return container;
+        }
+
+        /// <summary>
+        /// Registers the data access classes.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        /// <returns></returns>
+        public static Container RegisterDataAccess(this Container container)
+        {
+            container.Register<IContext,HolidaysSQLiteContext>();
+            container.Register<IRepository<DbModels.Holiday>, SQLiteRepository>();
+            return container;
+        }
+
+        /// <summary>
+        /// Registers the services.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        /// <returns></returns>
+        public static Container RegisterServices(this Container container)
+        {
+            container.Register<IFileLoader, FileLoader>();
+            container.Register<IDataProvider, DataProvider>();
+            //container.Register<ICalculator, Calculator>();
             return container;
         }
 
