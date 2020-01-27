723647264f32c3046b2b2f07367c32d447df80d0
Daniel Urdaneta <dsurdaneta@gmail.com>
2020-01-27 09:47:31 +0100

    implemented DataProvider

--- Source/Services/DataProvider.cs
+++ Source/Services/DataProvider.cs
@@ -1,37 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using DsuDev.BusinessDays.DataAccess;
-using DsuDev.BusinessDays.Domain.Entities;
 using DsuDev.BusinessDays.Services.Interfaces;
+using DbModels = DsuDev.BusinessDays.DataAccess.Models;
+using DomainEntities = DsuDev.BusinessDays.Domain.Entities;
 
 namespace DsuDev.BusinessDays.Services
 {
     public class DataProvider : IDataProvider
     {
-        private readonly IRepository<Holiday> holidayRepository;
+        private readonly IRepository<DbModels.Holiday> holidayRepository;
+        private readonly IMapper mapper;
 
-        public DataProvider(IRepository<Holiday> repository)
+        public DataProvider(IRepository<DbModels.Holiday> repository, IMapper mapper)
         {
             this.holidayRepository = repository ?? throw new ArgumentNullException(nameof(repository));
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
         /// <inheritdoc />
-        public async Task<ICollection<Holiday>> GetAllHolidaysAsync()
+        public async Task<ICollection<DomainEntities.Holiday>> GetAllHolidaysAsync()
         {
-            return await this.holidayRepository.GetAllAsync().ConfigureAwait(false);
+            var dbHolidays = await this.holidayRepository.GetAllAsync().ConfigureAwait(false);
+            var domainHolidays = this.mapper.Map<ICollection<DomainEntities.Holiday>>(dbHolidays);
+            return domainHolidays;
         }
 
         /// <inheritdoc />
-        public ICollection<Holiday> GetHolidays(int year)
+        public ICollection<DomainEntities.Holiday> GetHolidays(int year)
         {
-            throw new NotImplementedException();
+            var dbHolidays = this.holidayRepository.Get(h => h.Year == year);
+            var domainHolidays = this.mapper.Map<ICollection<DomainEntities.Holiday>>(dbHolidays);
+            return domainHolidays;
         }
 
         /// <inheritdoc />
-        public ICollection<Holiday> GetHolidays(DateTime startDateTime, DateTime endDateTime)
+        public ICollection<DomainEntities.Holiday> GetHolidays(DateTime startDateTime, DateTime endDateTime)
         {
-            throw new NotImplementedException();
+            bool HolidayIsBetweenDates(DbModels.Holiday holiday) => holiday.HolidayDate.ToUniversalTime() >= startDateTime.ToUniversalTime()
+                                                           && holiday.HolidayDate.ToUniversalTime() <= endDateTime.ToUniversalTime();
+
+            var dbHolidays = this.holidayRepository.Get(HolidayIsBetweenDates);
+            var domainHolidays = this.mapper.Map<ICollection<DomainEntities.Holiday>>(dbHolidays);
+            return domainHolidays;
         }
     }
 }
