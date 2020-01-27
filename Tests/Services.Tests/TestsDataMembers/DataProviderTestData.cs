0cf3dc7bc86a7c293b1f6e4c3748ff823d6d0a19
Daniel Urdaneta <dsurdaneta@gmail.com>
2020-01-27 15:48:07 +0100

    added tests and tests data class for DataProvider service

---
+++ Tests/Services.Tests/TestsDataMembers/DataProviderTestData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AutoMapper;
+using DsuDev.BusinessDays.DataAccess;
+using Moq;
+using DbModels = DsuDev.BusinessDays.DataAccess.Models;
+using DomainEntities = DsuDev.BusinessDays.Domain.Entities;
+
+namespace DsuDev.BusinessDays.Services.Tests.TestsDataMembers
+{
+    public class DataProviderTestData : IEnumerable<object[]>
+    {
+        /// <inheritdoc />
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            var mockMapper = new Mock<IMapper>();
+            var mockHolidayRepository = new Mock<IRepository<DbModels.Holiday>>();
+
+            yield return new object[] { null, mockHolidayRepository.Object };
+            yield return new object[] { mockMapper.Object, null };
+        }
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
