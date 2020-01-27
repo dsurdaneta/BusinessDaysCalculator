94947ddf4bc9ee9b05fbfed8cf6e980a5ee68761
Daniel Urdaneta <dsurdaneta@gmail.com>
2020-01-27 15:30:44 +0100

    added FileLoader service Unit Tests

---
+++ Tests/Services.Tests/TestsDataMembers/FileLoaderTestData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AutoMapper;
+using DsuDev.BusinessDays.Services.Interfaces.FileHandling;
+using Moq;
+
+namespace DsuDev.BusinessDays.Services.Tests.TestsDataMembers
+{
+    public class FileLoaderTestData : IEnumerable<object[]>
+    {
+        /// <inheritdoc />
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            var mockMapper = new Mock<IMapper>();
+            var mockFileReadingManager = new Mock<IFileReadingManager>();
+
+            yield return new object[] { null, mockFileReadingManager.Object};
+            yield return new object[] { mockMapper.Object, null };
+        }
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
