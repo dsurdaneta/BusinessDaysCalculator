7949362fcd4fe33396df59e460bb9972330dbf25
Daniel Urdaneta <dsurdaneta@gmail.com>
2020-01-28 12:53:52 +0100

    Implemented DataProvider unit tests and added baseId for generators

--- Tests/Services.Tests/DataProviderTests.cs
+++ Tests/Services.Tests/DataProviderTests.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AutoMapper;
+﻿using AutoMapper;
+using DsuDev.BusinessDays.Common.Tools;
+using DsuDev.BusinessDays.Common.Tools.SampleGenerators;
 using DsuDev.BusinessDays.DataAccess;
+using DsuDev.BusinessDays.Services.Tests.TestHelpers;
 using DsuDev.BusinessDays.Services.Tests.TestsDataMembers;
 using FluentAssertions;
 using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 using DbModels = DsuDev.BusinessDays.DataAccess.Models;
 using DomainEntities = DsuDev.BusinessDays.Domain.Entities;
@@ -23,6 +27,28 @@ namespace DsuDev.BusinessDays.Services.Tests
             this.mockHolidayRepository = new Mock<IRepository<DbModels.Holiday>>();
         }
 
+        public DataProvider SetuProvider(ICollection<DomainEntities.Holiday> domainHolidays, ICollection<DbModels.Holiday> dbHolidays)
+        {
+            this.mockMapper = new Mock<IMapper>();
+            this.mockHolidayRepository = new Mock<IRepository<DbModels.Holiday>>();
+
+            // setup mapping
+            this.mockMapper
+                .Setup(setup => setup.Map<ICollection<DomainEntities.Holiday>>(It.IsAny<ICollection<DbModels.Holiday>>()))
+                .Returns(domainHolidays);
+
+            this.mockMapper
+                .Setup(setup => setup.Map<DomainEntities.Holiday>(It.IsAny<DbModels.Holiday>()))
+                .Returns(domainHolidays.FirstOrDefault());
+
+            // setup repository methods
+            this.mockHolidayRepository.Setup(setup => setup.GetAllAsync()).Returns(Task.FromResult(dbHolidays));
+            this.mockHolidayRepository.Setup(setup => setup.Get(It.IsAny<Func<DbModels.Holiday, bool>> ()))
+                .Returns(dbHolidays);
+
+            return new DataProvider(this.mockHolidayRepository.Object, this.mockMapper.Object);
+        }
+
         [Theory]
         [ClassData(typeof(DataProviderTestData))]
         public static void Constructor_When_ParameterIsNull_Then_ThrowsException(
@@ -36,8 +62,77 @@ namespace DsuDev.BusinessDays.Services.Tests
             action.Should().Throw<ArgumentNullException>();
         }
 
-        // TODO tests for ICollection<DomainEntities.Holiday> GetHolidays(int year)
-        // TODO tests for ICollection<DomainEntities.Holiday> GetHolidays(DateTime startDateTime, DateTime endDateTime)
-        // TODO tests for async Task<ICollection<DomainEntities.Holiday>> GetAllHolidaysAsync()
+        [Fact]
+        public void GetHolidaysByYear_GetsCorrectlyTheHolidays()
+        {
+            // Arrange
+            const int baseYear = 2002;
+            const int initialAmount = 5;
+            
+            var domainHolidays = HolidayGenerator.CreateHolidays(initialAmount, baseYear);
+            var dbHolidays = HolidayGeneratorToolExtension.CreateDbHolidays(initialAmount, baseYear);
+            var otherHoliday = HolidayGeneratorToolExtension.CreateDbHoliday(
+                                RandomValuesGenerator.RandomInt(6, 25),
+                                year: baseYear + 2);
+            dbHolidays.Add(otherHoliday);
+
+            var provider = SetuProvider(domainHolidays, dbHolidays);
+
+            // Act
+            var sut = provider.GetHolidays(baseYear);
+
+            // Assert
+            sut.Should().NotBeNull();
+            sut.Count.Should().BeLessOrEqualTo(dbHolidays.Count);
+        }
+
+        [Fact]
+        public void GetHolidaysBetweenDates_GetsCorrectlyTheHolidays()
+        {
+            // Arrange
+            const int year = 1990;
+            
+            var startDate = new DateTime(year, 4, 26);
+            var endDateTime = new DateTime(year, 5, 11);
+
+            var provider = SetuProvider(
+                new List<DomainEntities.Holiday>
+                {
+                    HolidayGenerator.CreateHoliday(year)
+                },
+                new List<DbModels.Holiday>
+                {
+                    HolidayGeneratorToolExtension.CreateDbHoliday(RandomValuesGenerator.RandomInt(6), year)
+                });
+
+            // Act
+            var sut = provider.GetHolidays(startDate, endDateTime);
+
+            // Assert
+            sut.Should().NotBeNull();
+            sut.Count.Should().Be(1);
+        }
+        
+        [Fact]
+        public async void GetAllHolidaysAsync_GetsCorrectlyTheHolidays()
+        {
+            // Arrange
+            const int baseYear = 2002;
+            const int initialAmount = 5;
+            
+            var domainHolidays = HolidayGenerator.CreateHolidays(initialAmount, baseYear);
+            var dbHolidays = HolidayGeneratorToolExtension.CreateDbHolidays(initialAmount, baseYear);
+            var expectedFirstHoliday = domainHolidays.FirstOrDefault();
+            var provider = SetuProvider(domainHolidays, dbHolidays);
+
+            // Act
+            var sut = await provider.GetAllHolidaysAsync().ConfigureAwait(false);
+
+            // Assert
+            sut.Should().NotBeNull();
+            sut.Count.Should().Be(dbHolidays.Count);
+            sut.Any(x => x.Name.Equals(expectedFirstHoliday.Name)).Should().BeTrue();
+            sut.Any(x => x.HolidayDate.Equals(expectedFirstHoliday.HolidayDate)).Should().BeTrue();
+        }
     }
 }
