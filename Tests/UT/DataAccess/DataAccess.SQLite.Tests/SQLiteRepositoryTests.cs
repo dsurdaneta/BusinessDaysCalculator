e1f231dfb57d90ac27eec5969d34d1fc4ac60fbb
Daniel Urdaneta <dsurdaneta@gmail.com>
2020-01-30 12:37:35 +0100

    added empty test proj

---
+++ Tests/UT/DataAccess/DataAccess.SQLite.Tests/SQLiteRepositoryTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DsuDev.BusinessDays.DataAccess.SQLite.Tests
+{
+    public class SQLiteRepositoryTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
