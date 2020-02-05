14e37fbb7d71f89ed391772614f2690489554db3
Daniel Urdaneta <daniel.urdaneta@contractors.roche.com>
2020-02-05 11:37:08 +0100

    installed moq and added a context field

--- Tests/UT/DataAccess/DataAccess.SQLite.Tests/SQLiteRepositoryTests.cs
+++ Tests/UT/DataAccess/DataAccess.SQLite.Tests/SQLiteRepositoryTests.cs
@@ -1,10 +1,13 @@
 using System;
+using Moq;
 using Xunit;
 
 namespace DsuDev.BusinessDays.DataAccess.SQLite.Tests
 {
     public class SQLiteRepositoryTests
     {
+        private Mock<HolidaysSQLiteContext> contextMock;
+
         [Fact]
         public void Test1()
         {
