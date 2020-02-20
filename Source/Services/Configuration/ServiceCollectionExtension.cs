e2300f19b0035becea2afded44501eb0a65bc2ff
Daniel Urdaneta <urdanetd@emea.roche.com>
2020-02-20 14:06:45 +0100

    created AddThirdParty method

--- Source/Services/Configuration/ServiceCollectionExtension.cs
+++ Source/Services/Configuration/ServiceCollectionExtension.cs
@@ -1,11 +1,12 @@
-﻿using DsuDev.BusinessDays.Services.FileHandling;
+﻿using DsuDev.BusinessDays.DataAccess;
+using DsuDev.BusinessDays.DataAccess.SQLite;
+using DsuDev.BusinessDays.Services.FileHandling;
 using DsuDev.BusinessDays.Services.Interfaces;
 using DsuDev.BusinessDays.Services.Interfaces.FileHandling;
 using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics.CodeAnalysis;
-using DsuDev.BusinessDays.DataAccess;
-using DsuDev.BusinessDays.DataAccess.SQLite;
-
+using AutoMapper;
+using DsuDev.BusinessDays.Services.Profiles;
 using DbModels = DsuDev.BusinessDays.DataAccess.Models;
 
 namespace DsuDev.BusinessDays.Services.Configuration
@@ -13,14 +14,40 @@ namespace DsuDev.BusinessDays.Services.Configuration
     [ExcludeFromCodeCoverage]
     public static class ServiceCollectionExtension
     {
+        /// <summary>Adds all business days services.</summary>
+        /// <param name="services">The services.</param>
+        /// <returns></returns>
+        public static IServiceCollection AddAllBusinessDays(this IServiceCollection services)
+        {
+            return services.AddThirdParty()
+                            .AddBusinessDaysDataAccess()
+                            .AddBusinessDaysFileReaders()
+                            .AddBusinessDaysServices();
+        }
+
+        public static IServiceCollection AddThirdParty(this IServiceCollection services)
+        {
+            // Auto Mapper Configurations
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new HolidayContractProfile());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            return services;
+        }
+        
         /// <summary>Adds the Business Days services.</summary>
         /// <param name="services">The services.</param>
         /// <returns></returns>
         public static IServiceCollection AddBusinessDaysServices(this IServiceCollection services)
         {
-            services.AddSingleton(typeof(IFileLoader), typeof(FileLoader));
+            services.AddScoped(typeof(IFileLoader), typeof(FileLoader));
             services.AddSingleton(typeof(IDataProvider), typeof(DataProvider));
-            services.AddSingleton(typeof(ICalculator), typeof(Calculator));
+            services.AddScoped(typeof(ICalculator), typeof(Calculator));
+
             return services;
         }
 
@@ -31,19 +58,22 @@ namespace DsuDev.BusinessDays.Services.Configuration
         {
             services.AddSingleton(typeof(IContext), typeof(HolidaysSQLiteContext));
             services.AddSingleton(typeof(IRepository<DbModels.Holiday>), typeof(SQLiteRepository));
+
             return services;
         }
-        
+
+        /// <summary>Adds the business days file readers.</summary>
+        /// <param name="services">The services.</param>
+        /// <returns></returns>
         public static IServiceCollection AddBusinessDaysFileReaders(this IServiceCollection services)
         {
-            services.AddSingleton(typeof(IJsonReader), typeof(JsonHolidayReader));
-            services.AddSingleton(typeof(IXmlReader), typeof(XmlHolidayReader));
-            services.AddSingleton(typeof(ICsvHolidayReader), typeof(CsvHolidayReader));
-            services.AddSingleton(typeof(ICustomTxtReader), typeof(CustomTxtHolidayReader));
-            services.AddSingleton(typeof(IFileReadingManager), typeof(FileReadingManager));
+            services.AddTransient(typeof(IJsonReader), typeof(JsonHolidayReader));
+            services.AddTransient(typeof(IXmlReader), typeof(XmlHolidayReader));
+            services.AddTransient(typeof(ICsvHolidayReader), typeof(CsvHolidayReader));
+            services.AddTransient(typeof(ICustomTxtReader), typeof(CustomTxtHolidayReader));
+            services.AddTransient(typeof(IFileReadingManager), typeof(FileReadingManager));
+
             return services;
         }
-
-
     }
 }
